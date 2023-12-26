63aaab326d2c6e98b0ec14ba16608a31f781db94
HRJHL <125470712+HRJHL@users.noreply.github.com>
2023-12-26 19:48:30 +0900

    Delete tom directory

--- tom/app/src/main/java/com/example/tom/BlogResponse.kt
+++
@@ -1,3 +0,0 @@
-package com.example.tom
-
-data class BlogResponse(val status: String, val message: String?)
--- tom/app/src/main/java/com/example/tom/LoginResponse.kt
+++
@@ -1,3 +0,0 @@
-package com.example.tom
-
-data class LoginResponse(val status: String, val message: String?)
